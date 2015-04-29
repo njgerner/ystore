0b09405a1dc4d8eea78a26dff2351b35631bcec4
Phil Wertheimer <phil.wertheimer@gmail.com>
2015-04-28 22:24:16 -0400

    pulling nicks beef

--- assets/angular_front/search_results/searchresultsctrl.js
+++ assets/angular_front/search_results/searchresultsctrl.js
@@ -5,12 +5,26 @@ superApp.controller('SearchResultsCtrl',
 
     $scope.displayProducts = function(products) {
     	$scope.products = products;
+      $scope.displayedProducts = $scope.products.slice(0,15);
     }
 
     $scope.goToProduct = function(productnumber) {
       $state.go('product', {productnumber: productnumber});
     }
 
-	storeService.getFilteredProducts($scope.query.toLowerCase(), function(products) { $scope.displayProducts(products); });
+    //i think this will work but i can't test it with so few products.... guess we'll find out in a few months!
+    $scope.loadMore = function() {
+      var last = $scope.products[$scope.displayedProducts.length - 1];
+      for(var i = 1; i <= 6; i++) {
+        $scope.products.push(last + i);
+      }
+    }
+
+    if($scope.query) {
+       storeService.getFilteredProducts($scope.query.toLowerCase(), function(products) { $scope.displayProducts(products); });
+    } else {
+      $scope.allProducts = [];
+      $scope.dispayedProducts = [];
+    }
 
 }]);
