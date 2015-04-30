e6f649fba95ab46295638551e393fd8662ccf040
Nick Gerner <nick@workaroundcapital.com>
2015-04-29 21:58:33 -0400

    pushing master to heroku

---
+++ assets/angular_front/directives/mobile-nav-bar/mobile_nav_bar_dir.js
@@ -0,0 +1,14 @@
+appDirectives.directive('mobileNavBarDir', [
+	function() {
+	return {
+		restrict: 'E',
+		scope: {
+
+		},
+		templateUrl: 'directives/mobile_nav_bar_template.html',
+		link: function(scope, element) {
+			
+
+		}
+	}
+}]);
