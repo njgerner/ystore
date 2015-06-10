e5655b9a43e596b0a9c2ac9a931e1095414782b9
Nick Gerner <nick@workaroundcapital.com>
2015-06-10 19:51:43 -0400

    fixed hammer press on submit order

--- assets/angular_front/checkout/checkoutctrl.js
+++ assets/angular_front/checkout/checkoutctrl.js
@@ -71,6 +71,9 @@ superApp.controller('CheckoutCtrl',
     }
 
     $scope.submitOrder = function() {
+      if ($scope.orderSubmitted == true) {
+        return;
+      }
       $scope.orderSubmitted = true;
       stripeService.submitOrder($scope.addressShipTo, $scope.productsInCart, $scope.merchants, $scope.shippingCost, $scope.total, function(err, result) {
         if (err) {
