8fca2af9455eabd6c56f993b2fa7d2f304b2af23
Phil Wertheimer <phil.wertheimer@gmail.com>
2015-06-16 17:51:30 -0400

    reset pw form + feedback

--- assets/angular_front/reset_password/resetpasswordctrl.js
+++ assets/angular_front/reset_password/resetpasswordctrl.js
@@ -12,13 +12,24 @@ superApp.controller('ResetPasswordCtrl',
     });
 
     $scope.updatePassword = function() {
+      if(typeof $scope.password == "undefined" || $scope.password.length < 6) {
+        $scope.failedMessage = "Password must be at least 6 characters";
+        return;
+      }else if(typeof $scope.confirm == "undefined" || $scope.confirm.length == 0) {
+        $scope.failedMessage = "Please confirm your password";
+        return;
+      }else if($scope.password != $scope.confirm) {
+        $scope.failedMessage = "Passwords do not match";
+        return;
+      } else {
       authService.updatePassword($stateParams.resettoken, $scope.password, function(failedMessage, successMessage) {
-        if (successMessage) {
-          $state.go('login');
-        } else {
-          $scope.failedMessage = failedMessage;
-        }
-      })
+          if (successMessage) {
+            $state.go('login');
+          } else {
+            $scope.failedMessage = failedMessage;
+          }
+        });
+      }
     };
     
 }]);
