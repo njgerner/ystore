90ffcc26679e2ce0774e9470daa48fb664556771
Phil Wertheimer <phil.wertheimer@gmail.com>
2015-06-16 23:39:02 -0400

    product card directive

---
+++ assets/angular_front/directives/product_card/product_card_dir.js
@@ -0,0 +1,21 @@
+appDirectives.directive('productCardDir', [ '$state', '$rootScope', '$window', 'storeService',
+	function($state, $rootScope, $window, storeService) {
+	return {
+		restrict: 'E',
+		scope: {
+			product: '='
+		},
+		templateUrl: 'directives/product_card_template.html',
+		controller: function($scope) {
+		},
+		link: function(scope, element) {
+			scope.defaultImage = function(product) {
+		      if (product.remote_img) {
+		        product.img = product.remote_img;
+		      } else {
+		        product.img = "http://placehold.it/475x475&text=[img]";
+		      }
+		    }
+		}
+	}
+}]);
--- assets/angular_front/store/categories/ygearctrl.js
+++
@@ -1,5 +0,0 @@
-superApp.controller('YgearCtrl',
-  ['$rootScope', '$scope', '$window', '$location', '$state', '$stateParams', 'storeService',
-  function($rootScope, $scope, $window, $location, $state, $stateParams, storeService) {
-
-}]);
