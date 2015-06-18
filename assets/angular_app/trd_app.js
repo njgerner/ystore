b6359c05a13e692686844e8514941925c1820f0a
Phil Wertheimer <phil.wertheimer@gmail.com>
2015-06-18 17:27:07 -0400

    cr edit

--- assets/angular_app/trd_app.js
+++ assets/angular_app/trd_app.js
@@ -32,7 +32,7 @@ trdApp.run(['$rootScope', '$state', '$stateParams', '$cookies', '$location', 'au
           console.log('authorized / received', authService.authorized, authService.authorizationReceived);
 
           var isExceptionalState = function() {
-            var exceptionalState = ["terms", "store", "store.search", "checkout", "order", "support", "locations"];
+            var exceptionalState = ["terms", "store", "checkout", "order", "support", "locations"];
             return exceptionalState.indexOf(toState.name) >= 0;
           }
 
