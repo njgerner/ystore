eb3873a494a990454ee8378a8f902d1629798afc
Nick Gerner <nick@workaroundcapital.com>
2015-07-05 12:14:21 -0400

    removed merge error, added search abstract method

--- trd_modules/orchestrateHelper.js
+++ trd_modules/orchestrateHelper.js
@@ -1349,6 +1349,22 @@ exports.getDocFromCollection = function(collection, key) {
   return deferred.promise;
 };
 
+exports.searchDocsFromCollection = function(collection, query, params) {
+  var deferred = Q.defer();
+  db.search(collection, query, params)
+  .then(function (result){
+    deferred.resolve(rawDogger.push_values_to_top(result.body.results));
+  })
+  .fail(function (err) {
+    if (err.body.code == "items_not_found") {
+      deferred.resolve(false);
+    } else {
+      deferred.reject(new Error(err.body.message));
+    }
+  });
+  return deferred.promise;
+};
+
 exports.postDocToCollection = function(collection, doc) {
   var deferred = Q.defer();
   db.post(collection, doc)
